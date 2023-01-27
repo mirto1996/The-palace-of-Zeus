79f9de015ec2211f5b3f746bfa2bf88b32372715
fanismit <47718787+fanismit@users.noreply.github.com>
2023-01-27 19:25:36 +0200

    Φόρμα Εισαγωγής και νέα φόρμα εγγραφής

---
+++ The palace of Zeus/WelcomePage.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace The_palace_of_Zeus
+{
+    partial class WelcomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WelcomePage));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(995, 571);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 60);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Είσοδος";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(68, 571);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 60);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Κλείσιμο";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(146, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(958, 73);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Καλώς ήρθατε στο παλάτι του Δία";
+            // 
+            // WelcomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1233, 657);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WelcomePage";
+            this.Text = "WelcomePage";
+            this.Load += new System.EventHandler(this.WelcomePage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button2;
+        private Button button1;
+        private Label label1;
+    }
+}
---
+++ The palace of Zeus/WelcomePage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Media.SpeechSynthesis;
+namespace The_palace_of_Zeus
+{
+    public partial class WelcomePage : Form
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void WelcomePage_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RegistryAndSigning newpage= new RegistryAndSigning();
+            newpage.Show();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+
+        }
+    }
+}
