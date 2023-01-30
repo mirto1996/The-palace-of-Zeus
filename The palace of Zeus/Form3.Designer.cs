e29c1bce36350ecf4ff2d7366c669aa83c3e59fb
m.vergeti <myrtover@hotmail.com>
2023-01-30 23:08:59 +0200

    διαχειριση εξωτερικης πισινας

---
+++ The palace of Zeus/Form3.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace The_palace_of_Zeus
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Γέμισμα = new System.Windows.Forms.Button();
+            this.Άδειασμα = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.Άδειασμα);
+            this.panel1.Controls.Add(this.Γέμισμα);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(207, 460);
+            this.panel1.TabIndex = 0;
+            // 
+            // Γέμισμα
+            // 
+            this.Γέμισμα.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Γέμισμα.Location = new System.Drawing.Point(33, 65);
+            this.Γέμισμα.Name = "Γέμισμα";
+            this.Γέμισμα.Size = new System.Drawing.Size(133, 58);
+            this.Γέμισμα.TabIndex = 0;
+            this.Γέμισμα.Text = "Γέμισμα";
+            this.Γέμισμα.UseVisualStyleBackColor = true;
+            // 
+            // Άδειασμα
+            // 
+            this.Άδειασμα.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Άδειασμα.Location = new System.Drawing.Point(33, 164);
+            this.Άδειασμα.Name = "Άδειασμα";
+            this.Άδειασμα.Size = new System.Drawing.Size(133, 52);
+            this.Άδειασμα.TabIndex = 1;
+            this.Άδειασμα.Text = "Άδειασμα";
+            this.Άδειασμα.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(16, 296);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(172, 152);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(36, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "25°C";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("flowLayoutPanel1.BackgroundImage")));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(405, 39);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(749, 456);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1240, 507);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form3";
+            this.Text = "Διαχείριση εξωτερικής πισίνας";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private Button Άδειασμα;
+        private Button Γέμισμα;
+        private FlowLayoutPanel flowLayoutPanel1;
+    }
+}
